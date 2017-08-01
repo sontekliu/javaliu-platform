389cfeed79e3d4b0e604d6f8011a3b2678363c3b
sontek <sontek@yeah.net>
2017-08-01 11:46:33 +0800

    add some class

---
+++ platform-base/src/main/java/com/javaliu/platform/Global.java
@@ -0,0 +1,16 @@
+/**
+ * <br> 项目名：javaliu-platform
+ * <br> 文件名：Global.java
+ * <br> Copyright 2017 Javaliu
+ */
+package com.javaliu.platform;
+
+/**
+ * <br> 类 名：Global
+ * <br> 描 述：全局变量
+ * <br> 作 者：liushijun
+ * <br> 创 建：2017年08月01日
+ * <br> 版 本：V1.0
+ */
+public class Global {
+}
---
+++ platform-sdk/src/main/java/com/javaliu/plantform/T.java
@@ -0,0 +1,16 @@
+/**
+ * <br> 项目名：javaliu-platform
+ * <br> 文件名：T.java
+ * <br> Copyright 2017 国信嘉宁数据技术有限公司
+ */
+package com.javaliu.plantform;
+
+/**
+ * <br> 类 名：T
+ * <br> 描 述：TODO
+ * <br> 作 者：liushijun
+ * <br> 创 建：2017年08月01日
+ * <br> 版 本：TODO
+ */
+public class T {
+}
